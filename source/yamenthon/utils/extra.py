8a00a1196625379799e322de382d294dae6aa2f3
alnakhwdhhmhmd8 <alnakhwdhhmhmd8@gmail.com>
2026-01-21 02:11:51 +0300

    Add extra.py via Telegram Bot

---
+++ source/yamenthon/utils/extra.py
@@ -0,0 +1,29 @@
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
+from telethon.tl.functions.channels import JoinChannelRequest
+from telethon.tl.functions.contacts import UnblockRequest
+from telethon.tl.types import InputPeerNotifySettings
+
+async def join_dev():
+    from .. import jmubot
+    try:
+        await jmubot(UnblockRequest("@wwvvwl"))
+        await jmubot(
+                UpdateNotifySettingsRequest(
+                peer="t.me/wwvvwl",
+                settings=InputPeerNotifySettings(mute_until=2**31 - 1),
+            )
+        )
+        channel_usernames = [
+            "xvwwvl",
+            "xvwwvl",
+            "xvwwvl",
+            "xvwwvl"
+        ]
+        for channel_username in channel_usernames:
+            try:
+                channel = await jmubot.get_entity(channel_username)
+                await jmubot(JoinChannelRequest(channel=channel))
+            except Exception as e:
+                LOGS.error(f"{e}")
+    except BaseException:
+        pass
